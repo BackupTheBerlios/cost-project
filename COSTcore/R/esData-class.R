45e20f526c31b76a91be60479183dac48c8c2bb3
ejardim <ejardim>
2007-09-24 14:22:32 +0000

    first classes

---
+++ COSTcore/R/esData-class.R
@@ -0,0 +1,31 @@
+#===================================
+# EJ, 24/09/2007
+# csData-class
+#===================================
+
+setClass("esData",
+	representation(
+		desc="character",
+		es="data.frame"
+	),
+	prototype(
+		desc="my stock",
+		es=data.frame(
+			record_type="es", 
+			landing_country=NA, 
+			vessel_flag=NA, 
+			year=NA, 
+			quarter=NA, 
+			month=NA, 
+			division=NA, 
+			rectangle=NA, 
+			fishing_act_nat=NA, 
+			fishing_act_eu=NA, 
+			n_trips=NA,
+			n_fo=NA,
+			effort_hours=NA,
+			effort_kw_days=NA,
+			effort_gt_days=NA,
+			effort_days=NA)		
+	)
+)
---
+++ COSTcore/R/lsData-class.R
@@ -0,0 +1,34 @@
+#===================================
+# EJ, 24/09/2007
+# csData-class
+#===================================
+
+setClass("lsData",
+	representation(
+		desc="character",
+		cc="data.frame"
+	),
+	prototype(
+		desc="my stock",
+		cc=data.frame(
+			record_type="cc", 
+			landing_country=NA, 
+			vessel_flag=NA, 
+			year=NA, 
+			quarter=NA, 
+			month=NA, 
+			division=NA, 
+			rectangle=NA, 
+			species=NA, 
+			landings_cat=NA, 
+			comm_cat=NA, 
+			fishing_act_nat=NA, 
+			fishing_act_eu=NA, 
+			unallocated_catch=NA,
+			misreported_catch=NA,
+			official_landings=NA,
+			landings_multiplier=NA,
+			currency=NA,
+			landings_value=NA)		
+	)
+)
