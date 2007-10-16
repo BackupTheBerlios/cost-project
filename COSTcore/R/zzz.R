a3d23fb4c10cf8b13985203ba53b89b54c0e3f95
ejardim <ejardim>
2007-10-16 13:36:49 +0000

    iuiu

--- COSTcore/R/zzz.R
+++ COSTcore/R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(lib,pkg) {
 	cat("-----------------------------------------------------------------\n")
 	pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package="COSTcore"), fields=c("Version", "Built")))
-	cat("COSTcore - \"Zero\"\n")
+	cat("COSTcore - \"DoubleZero\"\n")
 	cat(paste("(Version: ", pkg.info["Version"], ". Built on: ", pkg.info["Built"], ")\n", sep=""))
 	cat("-----------------------------------------------------------------\n")
 }
