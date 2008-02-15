1bbb37f7152327c56955bc60cd83f0ce98351037
ejardim <ejardim>
2008-02-15 12:05:07 +0000

    RC 0

--- COSTcore/R/zzz.R
+++ COSTcore/R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(lib,pkg) {
 	cat("-------------------------------------------------------------------\n")
 	pkg.info <- drop(read.dcf(file=system.file("DESCRIPTION", package="COSTcore"), fields=c("Version", "Built")))
-	cat(" COSTcore - \"Beta\"\n")
+	cat(" COSTcore - \"One Release Candidate\"\n")
 	cat(paste(" (Version: ", pkg.info["Version"], ". Built on: ", pkg.info["Built"], ")\n", sep=""))
 	cat("-------------------------------------------------------------------\n")
 }
