949f8f16f581bd3aee9c5d157ddcadef78c3426a
mmerzere <mmerzere>
2008-10-07 07:56:04 +0000

    Analytical estimates of biological parameters

--- COSTdbe/R/dbe_Estimates_BioPar.r
+++ COSTdbe/R/dbe_Estimates_BioPar.r
@@ -1,41 +1,44 @@
 
-#########################################################
-#  Analytical estimates of empirical weight-at-length,  #
-# maturity-at-length, sex-ratio-at-length and variances #
-#########################################################
+#################################################################
+#    Analytical estimates of empirical weight-at-length/age,    #
+# maturity-at-length/age, sex-ratio-at-length/age and variances #
+#################################################################
 
 
 
-#library(COSTcore)
+library(COSTcore)
+
 
 
 setGeneric("bpEstim", function(dbeOutput,             #dbeOutput object
                                object,                #csDataCons object
-                               ...){
-
-  standardGeneric("bpEstim")
+                               adjust=TRUE,           #if FALSE, estimates at age are calculated directly fom ca table 
+                               ...){                  #     (length distribution in ca is representative of ld in the catch)
+                                                      #if TRUE, hl information is used to adjust estimates at age
+  standardGeneric("bpEstim")                          #     (length distribution in ca is not representative of ld in the catch)
 
 })
 
 
 
 
+
 setMethod("bpEstim", signature(dbeOutput="dbeOutput",object="csDataCons"), function(dbeOutput,
                                                                                     object,
+                                                                                    adjust=TRUE,
                                                                                     ...){
 
 As.num <- function(x) as.numeric(as.character(x))
 species <- dbeOutput@species
-fraction <- dbeOutput@catchCat ; if ("all"%in%fraction) fraction <- c("LAN","DIS")
 if (all(is.na(species))) stop("no species in 'dbeOutput' object!!")  
 if ("all"%in%species) species <- unique(as.character(c(object@ca$spp,object@hl$spp)))
 
 #-------------------------------------------------------------------------------
-#restriction of CA and HL tables to specified species & catch category
+#restriction of CA and HL tables to specified species (no restriction on catch category, all data is kept)
 #-------------------------------------------------------------------------------
 
 ca <- object@ca[object@ca$spp%in%species,]
-hl <- object@hl[object@hl$spp%in%species & extCatchCat(object@hl$sort)%in%fraction,]
+hl <- object@hl[object@hl$spp%in%species,]
 if (nrow(ca)==0) stop("no biological parameters in consolidated object!!")
 if (nrow(hl)==0) stop("no hl information!!")
 
@@ -65,68 +68,72 @@ bio <- switch(type,
 if (is.null(bio)) stop("wrong 'param' slot in 'dbeOutput' object!!")
 ca$bio <- bio
 
-#-------------------------------------------------------------------------------
-#creation of data.frame with numbers of measured fish by length class and strata
-#-------------------------------------------------------------------------------
-
-#numbers of measured fish per crossed strata
-NL <- tapply(hl$lenNum,list(paste(hl$lenCls,hl$time,hl$space,hl$technical,sep=":-:")),sum,na.rm=TRUE)  #":-:" separator mustn't be in used hl fields
-#and we build the data.frame from NL
-df <- as.data.frame(t(do.call("cbind",lapply(names(NL),function(x) strsplit(x,":-:")[[1]]))))
-df$nL <- NL ; names(df) <- c("lenCls","time","space","technical","nL")
-
-#-------------------------------------------------------------------------------
-#creation of data.frame with numbers sub-sampled by length class and strata
-#-------------------------------------------------------------------------------
-
-#numbers of measured fish per crossed strata
-ML <- tapply(1:nrow(ca),list(paste(ca$lenCls,ca$time,ca$space,ca$technical,sep=":-:")),length)  #":-:" separator mustn't be in used ca fields
-#and we build the data.frame from ML
-dF <- as.data.frame(t(do.call("cbind",lapply(names(ML),function(x) strsplit(x,":-:")[[1]]))))
-dF$mL <- ML ; names(dF) <- c("lenCls","time","space","technical","mL")
-
-#-------------------------------------------------------------------------------
-#creation of data.frame with numbers sub-sampled by age and strata
-#-------------------------------------------------------------------------------
-
-#numbers of measured fish per crossed strata
-MA <- tapply(1:nrow(ca),list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),length)  #":-:" separator mustn't be in used ca fields
-#and we build the data.frame from MA
-DF <- as.data.frame(t(do.call("cbind",lapply(names(MA),function(x) strsplit(x,":-:")[[1]]))))
-DF$mA <- MA ; names(DF) <- c("age","time","space","technical","mA")
-
-#-------------------------------------------------------------------------------
-#df, dF & DF tables are merged to ca
-#-------------------------------------------------------------------------------
-
-ca <- merge(ca,df,all.x=TRUE) ; ca$nL[is.na(ca$nL)] <- 0
-ca <- merge(ca,dF,all.x=TRUE)
-ca <- merge(ca,DF,all.x=TRUE)
-
-#-------------------------------------------------------------------------------
-#insertion of r field (r=nL/mL)
-#-------------------------------------------------------------------------------
-
-ca$r <- ca$nL/ca$mL
-
-#-------------------------------------------------------------------------------
-#sum r of A is put in ca
-#-------------------------------------------------------------------------------
-
-rSumA <- tapply(ca$r,list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),sum,na.rm=TRUE)  #":-:" separator mustn't be in used ca fields
-#and we build the data.frame from rSumA
-DFA <- as.data.frame(t(do.call("cbind",lapply(names(rSumA),function(x) strsplit(x,":-:")[[1]]))))
-DFA$rSumA <- rSumA ; names(DFA) <- c("age","time","space","technical","rSumA")
-#and merging is operated
-ca <- merge(ca,DFA,all.x=TRUE)
-
-#-------------------------------------------------------------------------------
-#calculation of statistical weights w
-#-------------------------------------------------------------------------------
-
-ca$w <- ca$mA*ca$r/ca$rSumA
 
 
+if (adjust) {
+
+  #-------------------------------------------------------------------------------
+  #creation of data.frame with numbers of measured fish by length class and strata
+  #-------------------------------------------------------------------------------
+
+  #numbers of measured fish per crossed strata
+  NL <- tapply(hl$lenNum,list(paste(hl$lenCls,hl$time,hl$space,hl$technical,sep=":-:")),sum,na.rm=TRUE)  #":-:" separator mustn't be in used hl fields
+  #and we build the data.frame from NL
+  df <- as.data.frame(t(do.call("cbind",lapply(names(NL),function(x) strsplit(x,":-:")[[1]]))))
+  df$nL <- NL ; names(df) <- c("lenCls","time","space","technical","nL")
+
+  #-------------------------------------------------------------------------------
+  #creation of data.frame with numbers sub-sampled by length class and strata
+  #-------------------------------------------------------------------------------
+
+  #numbers of measured fish per crossed strata
+  ML <- tapply(1:nrow(ca),list(paste(ca$lenCls,ca$time,ca$space,ca$technical,sep=":-:")),length)  #":-:" separator mustn't be in used ca fields
+  #and we build the data.frame from ML
+  dF <- as.data.frame(t(do.call("cbind",lapply(names(ML),function(x) strsplit(x,":-:")[[1]]))))
+  dF$mL <- ML ; names(dF) <- c("lenCls","time","space","technical","mL")
+
+  #-------------------------------------------------------------------------------
+  #creation of data.frame with numbers sub-sampled by age and strata
+  #-------------------------------------------------------------------------------
+
+  #numbers of measured fish per crossed strata
+  MA <- tapply(1:nrow(ca),list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),length)  #":-:" separator mustn't be in used ca fields
+  #and we build the data.frame from MA
+  DF <- as.data.frame(t(do.call("cbind",lapply(names(MA),function(x) strsplit(x,":-:")[[1]]))))
+  DF$mA <- MA ; names(DF) <- c("age","time","space","technical","mA")
+
+  #-------------------------------------------------------------------------------
+  #df, dF & DF tables are merged to ca
+  #-------------------------------------------------------------------------------
+
+  ca <- merge(ca,df,all.x=TRUE) ; ca$nL[is.na(ca$nL)] <- 0
+  ca <- merge(ca,dF,all.x=TRUE)
+  ca <- merge(ca,DF,all.x=TRUE)
+
+  #-------------------------------------------------------------------------------
+  #insertion of r field (r=nL/mL)
+  #-------------------------------------------------------------------------------
+
+  ca$r <- ca$nL/ca$mL
+
+  #-------------------------------------------------------------------------------
+  #sum r of A is put in ca
+  #-------------------------------------------------------------------------------
+
+  rSumA <- tapply(ca$r,list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),sum,na.rm=TRUE)  #":-:" separator mustn't be in used ca fields
+  #and we build the data.frame from rSumA
+  DFA <- as.data.frame(t(do.call("cbind",lapply(names(rSumA),function(x) strsplit(x,":-:")[[1]]))))
+  DFA$rSumA <- rSumA ; names(DFA) <- c("age","time","space","technical","rSumA")
+  #and merging is operated
+  ca <- merge(ca,DFA,all.x=TRUE)
+
+  #-------------------------------------------------------------------------------
+  #calculation of statistical weights w
+  #-------------------------------------------------------------------------------
+
+  ca$w <- ca$mA*ca$r/ca$rSumA
+
+}
 
 
 ################################################################################
@@ -136,19 +143,30 @@ ca$w <- ca$mA*ca$r/ca$rSumA
 MeanAtL <- tapply(ca$bio,list(ca$lenCls,ca$time,ca$space,ca$technical),mean,na.rm=TRUE)
 VarAtL <- tapply(ca$bio,list(ca$lenCls,ca$time,ca$space,ca$technical),var,na.rm=TRUE)
 
-MeanAtA <- tapply(ca$w*ca$bio,list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)/tapply(ca$w,list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)
-#MeanAtA is inserted in ca for variance calculation
-MatA <- tapply(ca$w*ca$bio,list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),sum,na.rm=TRUE)/tapply(ca$w,list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),sum,na.rm=TRUE)
-DFa <- as.data.frame(t(do.call("cbind",lapply(names(MatA),function(x) strsplit(x,":-:")[[1]]))))
-DFa$MatA <- MatA ; names(DFa) <- c("age","time","space","technical","MatA")
-#and merging is operated
-ca <- merge(ca,DFa,all.x=TRUE)
 
-VarAtA.Num <- tapply(ca$w*((ca$bio-ca$MatA)^2),list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)
-VarAtA.Den <- tapply(ca$w,list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)-1  
-VarAtA <- VarAtA.Num/VarAtA.Den
-#Nan values are in fact 0 values for variance (one fish in the stratum)
-VarAtA[is.nan(VarAtA)] <- 0
+if (adjust) {
+
+  MeanAtA <- tapply(ca$w*ca$bio,list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)/tapply(ca$w,list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)
+  #MeanAtA is inserted in ca for variance calculation
+  MatA <- tapply(ca$w*ca$bio,list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),sum,na.rm=TRUE)/tapply(ca$w,list(paste(ca$age,ca$time,ca$space,ca$technical,sep=":-:")),sum,na.rm=TRUE)
+  DFa <- as.data.frame(t(do.call("cbind",lapply(names(MatA),function(x) strsplit(x,":-:")[[1]]))))
+  DFa$MatA <- MatA ; names(DFa) <- c("age","time","space","technical","MatA")
+  #and merging is operated
+  ca <- merge(ca,DFa,all.x=TRUE)
+
+  VarAtA.Num <- tapply(ca$w*((ca$bio-ca$MatA)^2),list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)
+  VarAtA.Den <- tapply(ca$w,list(ca$age,ca$time,ca$space,ca$technical),sum,na.rm=TRUE)-1  
+  VarAtA <- VarAtA.Num/VarAtA.Den
+  #Nan values are in fact 0 values for variance (one fish in the stratum)
+  VarAtA[is.nan(VarAtA)] <- 0
+
+} else {
+
+  MeanAtA <- tapply(ca$bio,list(ca$age,ca$time,ca$space,ca$technical),mean,na.rm=TRUE)
+  VarAtA <- tapply(ca$bio,list(ca$age,ca$time,ca$space,ca$technical),var,na.rm=TRUE)
+
+}
+
 
 #dbeOutput must be updated, so result must be formatted
 formatt <- function(x,timeStrata,spaceStrata,techStrata,age=FALSE){
@@ -203,6 +221,7 @@ return(dbeOutput)
 #lSex <- bpEstim(dbeOutput3,object)
 #dbePlot(lSex,Slot="lenStruc",step=10,ylab="Sex ratio")
 #
+
 
 
 
