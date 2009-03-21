e1e7565b5bb38cabd6612e3ccf99bd3227396636
mmerzere <mmerzere>
2009-03-21 17:50:44 +0000

    *** empty log message ***

---
+++ COSTdbe/R/Raise_Age.r
@@ -0,0 +1,159 @@
+#data(sole)
+#strD <- strIni(timeStrata="quarter",techStrata="commCat")
+#csObject <- csDataCons(csDataVal(subset(sole.cs,sampType%in%c("M","V"))),strD)
+#clObject <- clDataCons(clDataVal(sole.cl),strD)
+#dbeOutput <- dbeObject(species="Solea solea",catchCat="LAN",strataDesc=strD)
+#sex <- as.character(NA)
+#
+#
+#spdAgreg <- COSTcore:::spdAgreg
+#
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+
+
+
+Raise_Age <- function(csObject,dbeOutput,type="fixed",sex=as.character(NA)){#type= "fixed" or "prop" or "ages"
+
+sp <- dbeOutput@species
+ca <- ca(csObject)
+#ca table is subset
+ca <- ca[ca$spp%in%sp,]   
+if (!(all(is.na(sex)))) ca <- ca[ca$sex%in%sex,]   
+
+#numbers at length
+Ldf <- dbeOutput@lenStruc$estim
+N <- tapply(Ldf$value,list(length=Ldf$length,time=Ldf$time,space=Ldf$space,technical=Ldf$technical),sum,na.rm=TRUE)
+
+#creating the stratified ALK with levels from N (duplication for technical strata)
+ALK <- tapply(ca$age,list(length=factor(ca$lenCls,levels=dimnames(N)[[1]]),age=ca$age,
+                          time=factor(ca$time,levels=dimnames(N)[[2]]),space=factor(ca$space,levels=dimnames(N)[[3]])),length)
+ALK[is.na(ALK)] <- 0
+  # --> duplication
+ll <- dimnames(ALK) ; ll[["technical"]] <- dimnames(N)[[4]]
+ALK <- array(rep(as.vector(ALK),dim(N)[4]),dim=c(dim(ALK),dim(N)[4]),dimnames=ll)
+
+  if (all(is.na(dbeOutput@lenStruc$estim))) stop("estimates for length structure are missing in 'dbeOutput' object!!")
+  if (all(is.na(dbeOutput@lenVar))) stop("variance for length structure is missing in 'dbeOutput' object!!")
+
+#nj : number of sampled individuals of length j
+  nj <- tapply(ca$lenCls,list(length=factor(ca$lenCls,levels=dimnames(N)[[1]]),time=factor(ca$time,levels=dimnames(N)[[2]]),
+                              space=factor(ca$space,levels=dimnames(N)[[3]])),function(x) sum(!is.na(x)))
+  nj[is.na(nj)] <- 0
+  # --> duplication
+  ll2 <- dimnames(nj) ; ll2[["technical"]] <- dimnames(N)[[4]]
+  nj <- array(rep(as.vector(nj),dim(N)[4]),dim=c(dim(nj),dim(N)[4]),dimnames=ll2)
+
+#Nl : number of length-sampled individuals
+	Nl <- apply(nj,2:4,sum)
+#ns : number of samples to be aged
+	ns <- apply(ALK,3:5,sum)
+#nl : number of sampled length categories
+  nl <- apply(nj,2:4,function(x) sum(x>0))
+#Q'ij
+	Qij <- aperm(aperm(ALK,c(1,3,4,5,2))/as.vector(apply(ALK,c(1,3:5),sum)),c(1,5,2:4))
+#njStar
+	if (type=="fixed") {
+    njStar <- ns/nl
+  } else {
+    njStar <- nj*rep(as.vector(ns/Nl),each=dim(nj)[1])}
+#lj : proportion of length j in the population
+	lj <- N/rep(as.vector(apply(N,2:4,sum,na.rm=TRUE)),each=dim(N)[1])
+  lj[is.na(lj)] <- 0
+
+#'pi' calculation
+#------------
+Pi.hat <- apply(aperm(aperm(Qij,c(1,3,4,5,2))*as.vector(lj),c(1,5,2,3,4)),2:5,sum,na.rm=TRUE)
+#test on Pi.hat (missing length class in ca)
+if (!all(apply(Pi.hat,2:4,sum,na.rm=TRUE)==1)) warning("it seems that some length classes from 'dbeOutput@lenStruc' slot are not in 'ca' table")
+
+#Var.pi calculation
+#----------------
+if (type=="ages") {
+	a1 <- Pi.hat*(1-Pi.hat)
+	VarPi <- a1/rep(ns,each=dim(a1)[1])
+} else {
+if (type=="fixed") {
+	b1 <- apply(aperm(aperm(Qij*(1-Qij),c(1,3,4,5,2))*as.vector(lj*(1-lj)),c(1,5,2,3,4)),2:5,sum,na.rm=TRUE)
+	b2 <- apply(aperm(aperm(Qij*(1-Qij),c(1,3,4,5,2))*as.vector(lj*lj),c(1,5,2,3,4)),2:5,sum,na.rm=TRUE)
+	b3 <- apply(aperm(aperm(Qij*Qij,c(1,3,4,5,2))*as.vector(lj),c(1,5,2,3,4)),2:5,sum,na.rm=TRUE)-Pi.hat^2
+  VarPi <- b1/rep(Nl*njStar,each=dim(b1)[1]) + b2/rep(njStar,each=dim(b2)[1]) + b3/rep(Nl,each=dim(b3)[1])
+} else {   #i.e if (type=="prop")
+	c1 <- apply(aperm(aperm(Qij*(1-Qij),c(1,3,4,5,2))*as.vector(lj),c(1,5,2,3,4)),2:5,sum,na.rm=TRUE)
+	c2 <- apply(aperm(aperm(Qij*Qij,c(1,3,4,5,2))*as.vector(lj),c(1,5,2,3,4)),2:5,sum,na.rm=TRUE)-Pi.hat^2
+  VarPi <- c1/rep(ns,each=dim(c1)[1]) + c2/rep(Nl,each=dim(c2)[1])
+}}
+
+#Estimates of total numbers at age
+  #total numbers
+D.hat <- apply(N,2:4,sum,na.rm=TRUE)
+  #total numbers at age
+D_i <- Pi.hat*rep(D.hat,each=dim(Pi.hat)[1])
+
+#Estimates of variance at age
+VarDj <- dbeOutput@lenVar
+  #Var(sum(D_j)) = sum(Var(D_j))
+VarD <- tapply(VarDj$value,list(time=factor(VarDj$time,levels=dimnames(N)[[2]]),space=factor(VarDj$space,levels=dimnames(N)[[3]]),
+                                technical=factor(VarDj$technical,levels=dimnames(N)[[4]])),sum,na.rm=TRUE)
+V1 <- Pi.hat*Pi.hat*rep(VarD,each=dim(Pi.hat)[1])
+V2 <- VarPi*rep(D.hat*D.hat,each=dim(VarPi)[1])
+V3 <- VarPi*rep(VarD,each=dim(VarPi)[1])
+
+VarD_i <- V1+V2+V3
+
+
+#results are inserted in dbeOutput object
+#####################
+  #D_i & VarD_i
+df.D_i <- cbind(expand.grid(dimnames(D_i)),value=as.vector(D_i))
+df.VarD_i <- cbind(expand.grid(dimnames(VarD_i)),value=as.vector(VarD_i))
+
+df.VarD_i <- df.VarD_i[!is.na(df.D_i$val),] ; df.D_i <- df.D_i[!is.na(df.D_i$val),] 
+df.VarD_i <- df.VarD_i[df.D_i$val>0,] ; df.D_i <- df.D_i[df.D_i$val>0,]
+  
+  #D_i
+df.D_i <- df.D_i[order(df.D_i$time,df.D_i$space,df.D_i$technical,df.D_i$age),] ; rownames(df.D_i) <- 1:nrow(df.D_i)
+dbeOutput@ageStruc$estim <- df.D_i[,names(dbeOutput@ageStruc$estim)]
+  
+  #VarD_j
+df.VarD_i <- df.VarD_i[order(df.VarD_i$time,df.VarD_i$space,df.VarD_i$technical,df.VarD_i$age),] ; rownames(df.VarD_i) <- 1:nrow(df.VarD_i)
+dbeOutput@ageVar <- df.VarD_i[,names(dbeOutput@ageVar)]
+
+return(dbeOutput)
+
+}
+
+
+
+###################
+# Exported method #
+###################
+
+
+
+setGeneric("RaiseAge", function(dbeOutput,
+                                 csObject,
+                                 type="fixed",
+                                 sex=as.character(NA),
+                                 ...){
+	standardGeneric("RaiseAge")}
+)
+
+
+
+setMethod("RaiseAge", signature(dbeOutput="dbeOutput",csObject="csDataCons"), function(dbeOutput,
+                                                                                       csObject,
+                                                                                       type="fixed",
+                                                                                       sex=as.character(NA),
+                                                                                       ...){
+                                                                                                           
+Raise_Age(csObject,dbeOutput,type=type,sex=sex)
+
+})
+
+
+
+
+
---
+++ COSTdbe/R/Raise_Lgth.r
@@ -0,0 +1,211 @@
+#data(sole)
+#strD <- strIni(timeStrata="quarter",techStrata="commCat")
+#csObject <- csDataCons(csDataVal(subset(sole.cs,sampType%in%c("M","V"))),strD)
+#clObject <- clDataCons(clDataVal(sole.cl),strD)
+#dbeOutput <- dbeObject(species="Solea solea",catchCat="LAN",strataDesc=strD)
+#sex <- as.character(NA)
+#
+#
+#spdAgreg <- COSTcore:::spdAgreg
+#
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+
+
+
+Raise_Lgth <- function(csObject,clObject,dbeOutput,spp,taxon,sex=as.character(NA)){
+
+sp <- dbeOutput@species
+if (missing(taxon)) taxon <- sp
+if (missing(spp)) spp <- sp   # todo : give as an argument the species in CS that correspond to taxon in CL
+
+# weight of the species/taxa/sex in the sampled box
+# this weight is to be calculated by summing the sampled weight by species/taxa/sex before subseting
+# and added as a third weight variable
+
+indNewWt <- TRUE
+if (is.na(sex) & all(taxon%in%sp)) indNewWt <- FALSE
+if (indNewWt) {
+
+  NW <- spdAgreg(list(newWt=csObject@sl$subSampWt),BY=list(PSUid=csObject@sl$PSUid,SSUid=csObject@sl$SSUid,TSUid=csObject@sl$TSUid,
+                                                         time=csObject@sl$time,space=csObject@sl$space,
+                                                         technical=csObject@sl$technical,sort=csObject@sl$sort),sum,na.rm=TRUE)
+
+  mergeSL <- merge(csObject@sl,NW,all.x=TRUE,sort=FALSE)
+  ssw <- csObject@sl$subSampWt
+  csObject@sl$subSampWt <- mergeSL$newWt
+  csObject@sl$lenCode <- ssw
+} else {
+  csObject@sl$lenCode <- csObject@sl$subSampWt
+}
+
+
+#subsetting "csObject" 
+csObject <- subsetSpp(csObject,spp%in%sp)
+if (!is.na(sex)) csObject <- subsetSpp(csObject,sex%in%sex)  #to keep all 'tr' and 'hh' information, we use "subsetSpp" method
+csObject <- subsetSpp(csObject,spp%in%sp)
+#subsetting "clObject"
+clObject <- subset(clObject,taxon%in%taxon)    
+
+SL <- csObject@sl
+HL <- csObject@hl
+#creating PSTUid field, concatenation of PSUid, SSUid, TSUid
+SL$PSTUid <- apply(SL[,c("PSUid","SSUid","TSUid")],1,function(x) paste(x,collapse=":-:"))
+HL$PSTUid <- apply(HL[,c("PSUid","SSUid","TSUid")],1,function(x) paste(x,collapse=":-:"))
+#creating STR field, concatenation of time, space, technical
+HHSTR <- apply(csObject@hh[,c("time","space","technical")],1,function(x) paste(x,collapse=":-:"))
+SL$STR <- apply(SL[,c("time","space","technical")],1,function(x) paste(x,collapse=":-:"))
+HL$STR <- apply(HL[,c("time","space","technical")],1,function(x) paste(x,collapse=":-:"))
+
+if (nrow(SL)==0) stop("the arguments given resulted in an empty table!!")
+
+#total landed weights per strata
+clObject@cl$landMult[is.na(clObject@cl$landMult)] <- 1
+#TotLand = OffLand*Multi + UnallocCat + MisallocCat
+totLand <- mapply(function(w,x,y,z) sum(c(w*x,y,z),na.rm=TRUE),clObject@cl$landWt,clObject@cl$landMult,clObject@cl$unallocCatchWt,clObject@cl$misRepCatchWt)
+totLandings <- spdAgreg(list(W=totLand),BY=list(time=clObject@cl$time,space=clObject@cl$space,technical=clObject@cl$technical),sum,na.rm=TRUE)
+
+
+#weight of the level
+wl <- tapply(SL$wt,list(STR=SL$STR,sort=SL$sort,TSUid=SL$TSUid,SSUid=SL$SSUid,PSUid=SL$PSUid),sum,na.rm=TRUE)   #reference for factor levels
+#sampled weight
+ws <- tapply(SL$subSampWt,list(STR=SL$STR,sort=SL$sort,TSUid=SL$TSUid,SSUid=SL$SSUid,PSUid=SL$PSUid),sum,na.rm=TRUE)
+#weight of the species/taxa/sex
+wt  <- tapply(as.numeric(as.character(SL$lenCode)),list(STR=SL$STR,sort=SL$sort,TSUid=SL$TSUid,SSUid=SL$SSUid,PSUid=SL$PSUid),sum,na.rm=TRUE)
+
+#number of fish in the sample by length
+d_j <- tapply(HL$lenNum,list(STR=factor(HL$STR,levels=levels(factor(SL$STR))),
+                              sort=factor(HL$sort,levels=levels(factor(SL$sort))),
+                              TSUid=factor(HL$TSUid,levels=levels(factor(SL$TSUid))),
+                              SSUid=factor(HL$SSUid,levels=levels(factor(SL$SSUid))),
+                              PSUid=factor(HL$PSUid,levels=levels(factor(SL$PSUid))),
+                              lenCls=HL$lenCls),sum,na.rm=TRUE)
+
+#TSUid stage
+d_jtsu <- apply(d_j*(as.vector(wl/ws)),c(1,3:6),sum,na.rm=TRUE)
+w_tsu <- apply(wt*wl/ws,c(1,3:5),sum,na.rm=TRUE)  # A REVOIR
+wl_tsu <- apply(wl,c(1,3:5),sum,na.rm=TRUE)
+
+#SSUid stage
+sum.d_jssu <- apply(d_jtsu,c(1,4,5),sum,na.rm=TRUE)
+sum.w_ssu <- apply(w_tsu,c(1,4),sum,na.rm=TRUE)
+sum.wl_ssu <- apply(wl_tsu,c(1,4),sum,na.rm=TRUE)
+
+#number of SSUid (total and sampled)
+Mi <- tapply(csObject@hh$SSUid,list(STR=factor(HHSTR,levels=levels(factor(SL$STR))),
+                                      PSUid=factor(csObject@hh$PSUid,levels=levels(factor(SL$PSUid)))),function(x) length(unique(x)))
+mi <- tapply(HL$SSUid,list(STR=factor(HL$STR,levels=levels(factor(SL$STR))),
+                           PSUid=factor(HL$PSUid,levels=levels(factor(SL$PSUid)))),function(x) length(unique(x)))
+
+#PSUid stage                           
+d_jpsu <- sum.d_jssu*as.vector(Mi/mi)
+w_psu <- sum.w_ssu*Mi/mi
+wl_psu <- sum.wl_ssu*Mi/mi
+
+#sum.wt <- apply(wt,1,sum,na.rm=TRUE)
+#sum.ws <- apply(ws,1,sum,na.rm=TRUE)
+
+sum.d_jpsu <- apply(d_jpsu,c(1,3),sum,na.rm=TRUE)
+sum.w_psu <- apply(w_psu,1,sum,na.rm=TRUE)
+sum.wl_psu <- apply(wl_psu,1,sum,na.rm=TRUE)
+
+W <- tapply(totLandings$W*1000,list(factor(apply(totLandings[,c("time","space","technical")],1,function(x) paste(x,collapse=":-:")),levels=dimnames(d_j)[[1]])),sum,na.rm=TRUE)
+
+D_j <- sum.d_jpsu*as.vector(W/sum.wl_psu)
+WHat <-  sum.w_psu*W/sum.wl_psu
+                                                                                                    
+
+
+## variance calculation based on 'Detecting sampling outliers and sampling heterogeneity when...' article (J. Vigneau & S. Mahévas)
+######################
+
+#n : number of sampled PSUid per strata
+n <- tapply(HL$PSUid,list(STR=factor(HL$STR,levels=levels(factor(SL$STR)))),function(x) length(unique(x)))
+
+first  <- W^2
+second <- (1-(sum.wl_psu/W))/(sum.wl_psu^2/n) 
+  third.1  <- sum.d_jpsu/as.vector(sum.wl_psu)
+  third.2 <- aperm(array(rep(as.vector(third.1),dim(w_psu)[2]),dim=dim(d_jpsu)[c(1,3,2)]),c(1,3,2))
+  third.3 <- d_jpsu-third.2*as.vector(wl_psu)
+third <- apply(third.3^2,c(1,3),sum,na.rm=TRUE)/as.vector(n-1)
+VarD_j <- third*as.vector(first*second)
+
+
+
+#results are inserted in dbeOutput object
+#####################
+  #D_j & VarD_j
+df.D_j <- cbind(expand.grid(dimnames(D_j)),value=as.vector(D_j))
+df.VarD_j <- cbind(expand.grid(dimnames(VarD_j)),value=as.vector(VarD_j))
+
+df.VarD_j <- df.VarD_j[!is.na(df.D_j$val),] ; df.D_j <- df.D_j[!is.na(df.D_j$val),] 
+df.VarD_j <- df.VarD_j[df.D_j$val>0,] ; df.D_j <- df.D_j[df.D_j$val>0,]
+  
+  #D_j
+df.D_j <- cbind(df.D_j,do.call("rbind",lapply(as.character(df.D_j$STR),function(x) strsplit(x,":-:")[[1]])))
+names(df.D_j) <- c("STR","length","value","time","space","technical")
+df.D_j <- df.D_j[order(df.D_j$time,df.D_j$space,df.D_j$technical,df.D_j$length),] ; rownames(df.D_j) <- 1:nrow(df.D_j)
+dbeOutput@lenStruc$estim <- df.D_j[,names(dbeOutput@lenStruc$estim)]
+  
+  #VarD_j
+df.VarD_j <- cbind(df.VarD_j,do.call("rbind",lapply(as.character(df.VarD_j$STR),function(x) strsplit(x,":-:")[[1]])))
+names(df.VarD_j) <- c("STR","length","value","time","space","technical")
+df.VarD_j <- df.VarD_j[order(df.VarD_j$time,df.VarD_j$space,df.VarD_j$technical,df.VarD_j$length),] ; rownames(df.VarD_j) <- 1:nrow(df.VarD_j)
+dbeOutput@lenVar <- df.VarD_j[,names(dbeOutput@lenVar)]
+
+  #WHat
+df.WHat <- cbind(value=WHat,as.data.frame(do.call("rbind",lapply(names(WHat),function(x) strsplit(x,":-:")[[1]]))))
+names(df.WHat) <- c("value","time","space","technical")
+df.WHat <- df.WHat[order(df.WHat$time,df.WHat$space,df.WHat$technical),] ; rownames(df.WHat) <- 1:nrow(df.WHat)
+dbeOutput@totalW$estim <- df.WHat[,names(dbeOutput@totalW$estim)]
+
+return(dbeOutput)
+
+}
+
+###################
+# Exported method #
+###################
+
+
+
+setGeneric("RaiseLgth", function(dbeOutput,
+                                 csObject,
+                                 clObject,
+                                 spp,
+                                 taxon,
+                                 sex=as.character(NA),
+                                 ...){
+	standardGeneric("RaiseLgth")}
+)
+
+
+
+setMethod("RaiseLgth", signature(dbeOutput="dbeOutput",csObject="csDataCons",clObject="clDataCons"), function(dbeOutput,
+                                                                                                              csObject,
+                                                                                                              clObject,
+                                                                                                              spp,
+                                                                                                              taxon,
+                                                                                                              sex=as.character(NA),
+                                                                                                              ...){
+                                                                                                           
+Raise_Lgth(csObject,clObject,dbeOutput,spp=spp,taxon=taxon,sex=sex)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             
