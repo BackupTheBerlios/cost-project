cb98c8cb4a502abd5097c0e84acce5821147dbdd
mmerzere <mmerzere>
2009-06-17 12:43:03 +0000

    *** empty log message ***

--- COSTmbe/R/mbe2dbe.R
+++ COSTmbe/R/mbe2dbe.R
@@ -1,3 +1,46 @@
+### function that fills 'estimates' and 'variance' tables in a COSTdbe object from 'replicates' table ###
+fillTabFromRep <- function(object){
+
+  ##subfunction
+  formEst <- function(tabRep,type="l",...){ #type = "l" (length) or "a" (age)
+  tabRep$value[is.infinite(tabRep$value) | is.nan(tabRep$value)] <- NA
+  if (type=="l") {
+    tabRep$length <- factor(as.character(tabRep$length),levels=as.character(unique(tabRep$length)))
+    tab <- with(tabRep,aggregate(value,list(length=length,technical=technical,space=space,time=time),...))
+    names(tab)[ncol(tab)] <- "value"
+  } else {
+    tabRep$age <- factor(as.character(tabRep$age),levels=as.character(unique(tabRep$age)))
+    tab <- with(tabRep,aggregate(value,list(age=age,technical=technical,space=space,time=time),...))
+    names(tab)[ncol(tab)] <- "value"
+  }
+  tab <- tab[,names(tabRep)[-ncol(tabRep)]]
+  return(tab)
+  }
+
+if (!all(is.na(object@lenStruc$rep))) {
+  object@lenStruc$estim <- formEst(object@lenStruc$rep,type="l",mean,na.rm=TRUE)
+  object@lenVar <- formEst(object@lenStruc$rep,type="l",var,na.rm=TRUE)
+}
+
+if (!all(is.na(object@ageStruc$rep))) {
+  object@ageStruc$estim <- formEst(object@ageStruc$rep,type="a",mean,na.rm=TRUE)
+  object@ageVar <- formEst(object@ageStruc$rep,type="a",var,na.rm=TRUE)
+}
+
+return(object)
+}
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+
+
+############################
+#### 'mbe2dbe' function ####
+############################
+
+
 mbe2dbe <- function(mbeoutput,species=as.character(NA)) {
 
 if (nrow(mbeoutput$cov)>1) {
@@ -31,33 +74,33 @@ atLgth <- apply(mbeoutput$totcatch.land,c(1,3),sum,na.rm=TRUE)
 atAge <- apply(mbeoutput$totcatch.land,2:3,sum,na.rm=TRUE)
 expLgth <- expand.grid(dimnames(atLgth)) ; expAge <- expand.grid(dimnames(atAge))
 
-dbeTotcatch.land@lenStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,length=as.character(expLgth[,1]),value=as.vector(atLgth),iter=expLgth[,2]))
-dbeTotcatch.land@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1]),value=as.vector(atAge),iter=expAge[,2]))
+dbeTotcatch.land@lenStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,length=as.character(expLgth[,1])),value=as.vector(atLgth),iter=expLgth[,2])
+dbeTotcatch.land@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1])),value=as.vector(atAge),iter=expAge[,2])
 
 #totcatch.disc
 atLgth <- apply(mbeoutput$totcatch.disc,c(1,3),sum,na.rm=TRUE)
 atAge <- apply(mbeoutput$totcatch.disc,2:3,sum,na.rm=TRUE)
 expLgth <- expand.grid(dimnames(atLgth)) ; expAge <- expand.grid(dimnames(atAge))
-dbeTotcatch.disc@lenStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,length=as.character(expLgth[,1]),value=as.vector(atLgth),iter=expLgth[,2]))
-dbeTotcatch.disc@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1]),value=as.vector(atAge),iter=expAge[,2]))
+dbeTotcatch.disc@lenStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,length=as.character(expLgth[,1])),value=as.vector(atLgth),iter=expLgth[,2])
+dbeTotcatch.disc@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1])),value=as.vector(atAge),iter=expAge[,2])
 
 #mean.lga.land
 expAge <- expand.grid(dimnames(mbeoutput$mean.lga.land))
-dbeMean.lga.land@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1]),value=as.vector(mbeoutput$mean.lga.land),iter=expAge[,2]))
+dbeMean.lga.land@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1])),value=as.vector(mbeoutput$mean.lga.land),iter=expAge[,2])
 
 #mean.wga.land
 expAge <- expand.grid(dimnames(mbeoutput$mean.wga.land))
-dbeMean.wga.land@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1]),value=as.vector(mbeoutput$mean.wga.land),iter=expAge[,2]))
+dbeMean.wga.land@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1])),value=as.vector(mbeoutput$mean.wga.land),iter=expAge[,2])
 
 #mean.lga.disc
 expAge <- expand.grid(dimnames(mbeoutput$mean.lga.disc))
-dbeMean.lga.disc@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1]),value=as.vector(mbeoutput$mean.lga.disc),iter=expAge[,2]))
+dbeMean.lga.disc@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1])),value=as.vector(mbeoutput$mean.lga.disc),iter=expAge[,2])
 
 #mean.wga.disc
 expAge <- expand.grid(dimnames(mbeoutput$mean.wga.disc))
-dbeMean.wga.disc@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1]),value=as.vector(mbeoutput$mean.wga.disc),iter=expAge[,2]))
+dbeMean.wga.disc@ageStruc$rep <- data.frame(cbind(time=time,space=space,technical=technical,age=as.character(expAge[,1])),value=as.vector(mbeoutput$mean.wga.disc),iter=expAge[,2])
 
-return(list(dbeTotcatch.land=dbeTotcatch.land,dbeTotcatch.disc=dbeTotcatch.disc,dbeMean.lga.land=dbeMean.lga.land,
-            dbeMean.wga.land=dbeMean.wga.land,dbeMean.lga.disc=dbeMean.lga.disc,dbeMean.wga.disc=dbeMean.wga.disc))
+return(list(dbeTotcatch.land=fillTabFromRep(dbeTotcatch.land),dbeTotcatch.disc=fillTabFromRep(dbeTotcatch.disc),dbeMean.lga.land=fillTabFromRep(dbeMean.lga.land),
+            dbeMean.wga.land=fillTabFromRep(dbeMean.wga.land),dbeMean.lga.disc=fillTabFromRep(dbeMean.lga.disc),dbeMean.wga.disc=fillTabFromRep(dbeMean.wga.disc)))
 }
 
