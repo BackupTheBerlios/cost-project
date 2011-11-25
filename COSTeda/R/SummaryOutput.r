f0aeb1df5124b5c244e9112cde46d0b7d473b6b0
mmerzere <mmerzere>
2011-11-25 09:33:03 +0000

    Summary "plus" & 'disInfo' & 'controlMap'

--- COSTeda/R/SummaryOutput.r
+++ COSTeda/R/SummaryOutput.r
@@ -576,3 +576,261 @@ return(df)
 }
 
 
+
+
+###############
+##  ctrlMap  ##
+###############
+
+
+
+#on teste 2 champs 
+testTk <- function(csField,othField) {
+
+  if (is.null(csField) | is.null(othField)) {
+  
+    return("-")
+  
+  } else {
+
+    ref <- as.character(csField)
+    comp <- as.character(othField)
+
+    allNA <- all(is.na(comp)) & all(is.na(ref))
+    naS <- !all(is.na(comp)) & all(is.na(ref))
+    nA <- all(is.na(comp)) & !all(is.na(ref))
+
+    if (!(allNA | naS | nA)) {
+
+      greater <- all(ref%in%comp)
+      smaller <- all(comp%in%ref)
+      equal <- greater & smaller
+      if (equal) {
+        greater <- smaller <- different <- FALSE
+      } else {
+        different <- (!greater) & (!smaller)
+      }
+    
+    } else {
+
+      greater <- smaller <- equal <- different <- FALSE
+    }
+
+  tst <- c("OK","<S",">S","!=S","NA-NA","X-NA","NA-X")[c(equal,smaller,greater,different,allNA,naS,nA)]
+  if (length(tst)>1) stop("!!!!")
+
+  return(tst)
+
+}
+
+}
+
+
+
+ctrlMap <- function(cs,ca,cl,ce) {
+
+#il faut commencer par insérer la donnée temporelle dans les tables
+cs@hh$Ymonth <- sapply(as.character(cs@hh$date),function(x) as.numeric(strsplit(x,"-")[[1]][2])) 
+cs@hh$Yquarter <- ceiling(cs@hh$Ymonth/3)
+cs@hh$Ysemester <- ceiling(cs@hh$Ymonth/6)
+cs@ca$semester <- ceiling(as.numeric(as.character(cs@ca$quarter))/2)
+#on colle l'année au champ trimestre, semestre et mois
+cs@hh$Ymonth <- paste(as.character(cs@hh$year),as.character(cs@hh$Ymonth),sep=" - ")
+cs@hh$Yquarter <- paste(as.character(cs@hh$year),as.character(cs@hh$Yquarter),sep=" - ")
+cs@hh$Ysemester <- paste(as.character(cs@hh$year),as.character(cs@hh$Ysemester),sep=" - ")
+cs@ca$month <- paste(as.character(cs@ca$year),as.character(cs@ca$month),sep=" - ")
+cs@ca$quarter <- paste(as.character(cs@ca$year),as.character(cs@ca$quarter),sep=" - ")
+cs@ca$semester <- paste(as.character(cs@ca$year),as.character(cs@ca$semester),sep=" - ")
+
+clInd <- !is.null(cl) ; ceInd <- !is.null(ce) ; caInd <- (nrow(ca)>1)
+if ((!clInd) & (!ceInd) & (!caInd)) stop("Nothing to check!!")
+
+if (clInd) {
+  cl@cl$semester <- ceiling(as.numeric(as.character(cl@cl$quarter))/2)
+  cl@cl$month <- paste(as.character(cl@cl$year),as.character(cl@cl$month),sep=" - ")
+  cl@cl$quarter <- paste(as.character(cl@cl$year),as.character(cl@cl$quarter),sep=" - ")
+  cl@cl$semester <- paste(as.character(cl@cl$year),as.character(cl@cl$semester),sep=" - ")
+}
+
+if (ceInd) {
+  ce@ce$semester <- ceiling(as.numeric(as.character(ce@ce$quarter))/2)
+  ce@ce$month <- paste(as.character(ce@ce$year),as.character(ce@ce$month),sep=" - ")
+  ce@ce$quarter <- paste(as.character(ce@ce$year),as.character(ce@ce$quarter),sep=" - ")
+  ce@ce$semester <- paste(as.character(ce@ce$year),as.character(ce@ce$semester),sep=" - ")
+}
+
+outp <- matrix("",ncol=3,nrow=14)
+
+#les champs à traiter sont :
+CSfield <- c("@sl$spp","@hh$year","@hh$Ysemester","@hh$Yquarter","@hh$Ymonth","@hh$area","@hh$rect","@hh$subRect",
+                "@hh$foCatNat","@hh$foCatEu5","@hh$foCatEu6","@sl$commCat","@sl$lenCode")
+CAfield <- c("@ca$spp","@ca$year","@ca$semester","@ca$quarter","@ca$month","@ca$area","@ca$rect","@ca$subRect",
+                "@ca$foCatNat","@ca$foCatEu5","@ca$foCatEu6","@ca$commCat","@ca$lenCode")
+CLfield <- c("@cl$taxon","@cl$year","@cl$semester","@cl$quarter","@cl$month","@cl$area","@cl$rect","@cl$subRect",
+                "@cl$foCatNat","@cl$foCatEu5","@cl$foCatEu6","@cl$commCat","@cl$lenCode")
+CEfield <- c("@ce$spp","@ce$year","@ce$semester","@ce$quarter","@ce$month","@ce$area","@ce$rect","@ce$subRect",
+                "@ce$foCatNat","@ce$foCatEu5","@ce$foCatEu6","@ce$commCat","@ce$lenCode")
+fields <- c("spp-taxon","year","semester","quarter","month","area","rect","subRect","foCatNat","foCatEu5","foCatEu6","commCat","lenCode","lenCls")
+
+
+
+
+
+require(tcltk)
+require(tcltk2)
+ttc <- tktoplevel()
+tkfocus(ttc) 
+tkwm.deiconify(ttc)
+tkgrab.set(ttc)
+tkwm.title(ttc, "Check fields consistency")
+fontHeading <- tkfont.create(family="times",size=14,weight="bold")
+fontSymb <- tkfont.create(family="times",size=13)
+eval(parse('',text=paste("
+tkgrid(tklabel(ttc,text=\"     \"), tklabel(ttc,text=\"     \"),","tklabel(ttc,text=\"     CA~CS     \",font=fontHeading),"[caInd],"tklabel(ttc,text=\"     CL~CS     \",font=fontHeading),"[clInd],
+                    "tklabel(ttc,text=\"     CE~CS     \",font=fontHeading),"[ceInd],"tklabel(ttc,text=\"     tabConsist     \",font=fontHeading))",sep="")))
+tkgrid(tklabel(ttc,text=""))
+tkgrid(tklabel(ttc,text=""))
+colFont <- c("green4","red","green4","red","black","black","black","black")
+colFont2 <- c("green4","black","green4","black","black","black","black","black")
+names(colFont) <- names(colFont2) <- c("OK","<S",">S","!=S","NA-NA","X-NA","NA-X","-")                    
+cbTab <- character(14)    
+tkfocus(ttc)   
+for (i in 1:(length(cbTab)-1)) {
+  eval(parse('',text=paste("cbF",i," <- tkcheckbutton(ttc)",sep="")))
+  eval(parse('',text=paste("cbValueF",i," <- tclVar(\"0\")",sep="")))
+  eval(parse('',text=paste("tkconfigure(cbF",i,",variable=cbValueF",i,")",sep="")))
+  if (caInd) eval(parse('',text=paste("res1 <- outp[i,1] <- testTk(cs",CSfield[i],",cs",CAfield[i],")",sep="")))
+  if (clInd) eval(parse('',text=paste("res2 <- outp[i,2] <- testTk(cs",CSfield[i],",cl",CLfield[i],")",sep="")))
+  if (ceInd) eval(parse('',text=paste("res3 <- outp[i,3] <- testTk(cs",CSfield[i],",ce",CEfield[i],")",sep="")))
+  if (i>1) {
+    eval(parse('',text=paste("tkgrid(tklabel(ttc,text=fields[",i,"],font=fontHeading),tklabel(ttc,text=\"\"),","tklabel(ttc,text=res1,foreground=colFont[res1],font=fontSymb),"[caInd],"tklabel(ttc,text=res2,foreground=colFont[res2],font=fontSymb),"[clInd],"tklabel(ttc,text=res3,foreground=colFont[res3],font=fontSymb),"[ceInd],"cbF",i,")",sep="")))
+  } else {
+    eval(parse('',text=paste("tkgrid(tklabel(ttc,text=fields[",i,"],font=fontHeading),tklabel(ttc,text=\"\"),","tklabel(ttc,text=res1,foreground=colFont[res1],font=fontSymb),"[caInd],"tklabel(ttc,text=res2,foreground=colFont2[res2],font=fontSymb),"[clInd],"tklabel(ttc,text=res3,font=fontSymb),"[ceInd],"cbF",i,")",sep="")))
+  }
+  if (i %in% c(1,5,8,11,12)) {
+    tkgrid(tklabel(ttc,text=""))
+    tkgrid(tklabel(ttc,text=""))
+  }
+}
+   
+#la dernière ligne est spécifique : analyse des classes de tailles (proportion de classes de taille dans CS présentes dans CA
+propLC <- unique(cs@hl$lenCls)%in%unique(ca$lenCls)
+prop <- round(100*sum(propLC)/length(propLC),1)
+
+outp[14,] <- c(paste(prop,"%",sep=""),"-","-") 
+cbF14 <- tkcheckbutton(ttc) 
+cbValueF14 <- tclVar("0")
+tkconfigure(cbF14,variable=cbValueF14)
+eval(parse('',text=paste("tkgrid(tklabel(ttc,text=\"lenCls\",font=fontHeading),tklabel(ttc,text=\"\"),",
+      "tklabel(ttc,text=paste(prop,\"%\",sep=\"\"),foreground=c(\"green4\",\"black\",\"red\")[c(prop>=90,(prop<90 & prop>=75),prop<75)],font=fontSymb),"[caInd],
+      "tklabel(ttc,text=\"-\",font=fontSymb),"[clInd],"tklabel(ttc,text=\"-\",font=fontSymb),"[ceInd],"cbF14)",sep="")))
+ 
+
+dimnames(outp) <- list(fields,c("CA~CS","CL~CS","CE~CS")) 
+
+ 
+   
+OnOK <- function()
+{
+    cs@hh$month <- cs@hh$Ymonth 
+    cs@hh$quarter <- cs@hh$Yquarter
+    cs@hh$semester <- cs@hh$Ysemester
+    fields[1] <- "spp"
+    for (i in 1:length(cbTab)) {
+      eval(parse('',text=paste("cbTab[",i,"] <<- as.character(tclvalue(cbValueF",i,"))",sep="")))
+      if (cbTab[i]=="1") {
+      print(fields[i])
+      try(eval(parse('',text=paste("tabC <- tabConsist(list(cs",",cl"[clInd],",ce"[ceInd],"),\"",fields[i],"\")",sep=""))),silent=TRUE) 
+      try(print(tabC),silent=TRUE)
+      tabC <- NA
+      cat("\n\n")
+      }
+    }
+
+    tkgrab.release(ttc)
+    tkdestroy(ttc)
+}
+
+OnCancel <- function() {
+
+    cbTab <<- rep("0",length(cbTab))
+    tkgrab.release(ttc)
+    tkdestroy(ttc)
+    
+    }
+    
+OK.but <- tkbutton(ttc, text = "   OK   ", command = OnOK)
+Cancel.but <- tkbutton(ttc, text = " Cancel ", command = OnCancel)
+tkgrid(tklabel(ttc,text=""))
+tkgrid(tklabel(ttc,text=""),tklabel(ttc,text=""),tklabel(ttc,text=""), tklabel(ttc,text=""),OK.but, Cancel.but)
+tkgrid(tklabel(ttc, text = "    "))
+tkfocus(ttc)  
+
+invisible(as.data.frame(outp)) 
+}  
+
+
+
+
+
+setGeneric("controlMap", function(csObject,
+                                 secObject,
+                                 thiObject,
+                                 ...){     #COSTeda:::tabConsist 's parameters                                                  
+	standardGeneric("controlMap")}
+)
+
+
+
+setMethod("controlMap", signature(csObject="csData",secObject="clData",thiObject="ceData"), function(csObject,
+                                                                                                              secObject,
+                                                                                                              thiObject,
+                                                                                                              ...){
+
+                                                                                                 
+ctrlMap(csObject,ca(csObject),secObject,thiObject,...)
+
+})
+
+setMethod("controlMap", signature(csObject="csData",secObject="clData",thiObject="missing"), function(csObject,
+                                                                                                              secObject,
+                                                                                                              ...){
+
+                                                                                                 
+ctrlMap(csObject,ca(csObject),secObject,NULL,...)
+
+})
+
+
+setMethod("controlMap", signature(csObject="csData",secObject="ceData",thiObject="missing"), function(csObject,
+                                                                                                              secObject,
+                                                                                                              ...){
+
+                                                                                                 
+ctrlMap(csObject,ca(csObject),NULL,secObject,...)
+
+})
+
+setMethod("controlMap", signature(csObject="csData",secObject="missing",thiObject="missing"), function(csObject,
+                                                                                                              ...){
+
+                                                                                                 
+ctrlMap(csObject,ca(csObject),NULL,NULL,...)
+
+})
+
+
+
+
+ 
+##exemple
+# data(sole)
+# gg <- controlMap(sole.cs,sole.cl,sole.ce)
+
+
+
+
+
+
+
+
